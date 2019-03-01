e3587bd3df45c46665d936765ef2df06c5c079f6
Nicholas Rodine <halfofastaple@gmail.com>
2019-03-01 17:53:52 -0500

    Gave the player a minimum jump height of about 1 block

--- Player/States/InAir.gd
+++ Player/States/InAir.gd
@@ -4,7 +4,8 @@ const run_speed = 48;
 
 func update(player: Player, _delta):
 	var input = player.input;
-	if (player.velocity.y < 0 && input.Fall):
+	var vert_vel = player.velocity.y;
+	if (vert_vel < 0 && vert_vel > -150 && input.Fall):
 		player.velocity.y = 0;
 	var velocity = (int(input.Right)-int(input.Left));
 	player.velocity.x = velocity * run_speed;
